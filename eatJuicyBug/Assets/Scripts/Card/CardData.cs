d3c08b0a5be4f162566af1afcf5dab88c79229fb
Zaidis <chewybinx@gmail.com>
2020-10-23 22:33:31 -0500

    CARDS :D
    We got cards naw

---
+++ eatJuicyBug/Assets/Scripts/Card/CardData.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardData : MonoBehaviour
+{
+    //Add as many effects into a card as you would like
+    public List<CardEffect> cardEffects = new List<CardEffect>();
+
+    public void Play() {
+        for(int i = 0; i < cardEffects.Count; i++) {
+            cardEffects[i].PlayCard();
+        }
+    }
+}
---
+++ eatJuicyBug/Assets/Scripts/Card/PlayCreature.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayCreature : CardEffect
+{
+
+    public override void PlayCard() {
+        GameManager.instance.PlayCreature();
+    }
+}

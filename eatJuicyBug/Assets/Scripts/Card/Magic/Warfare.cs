f1c22a5176ca6f0760953a451db982301ef4d292
Leah Blasczyk <BlasczykLeah@users.noreply.github.com>
2020-10-25 17:24:50 -0500

    hoping for the best

--- eatJuicyBug/Assets/Scripts/Card/Magic/Warfare.cs
+++ eatJuicyBug/Assets/Scripts/Card/Magic/Warfare.cs
@@ -7,13 +7,14 @@ public class Warfare : Magic
 
     // destroy each of creature type
     public override void DoMagic(int playerIndex, int targetIndex, CreatureType creatureType) {
-        for (int i = 0; i < GameManager.instance.tableLayouts.Length; i++) {
+        for (int i = 0; i < Networking.server.playerSockets.Count; i++) {
             foreach (GameObject card in GameManager.instance.tableLayouts[i].tableCards) {
                 if (card.GetComponent<CardData>().GetCreatureType() == creatureType) {
                     // if the player has the specific creature type. 
                     GameManager.instance.tableLayouts[i].GetComponent<CustomLayout>().removePlacedCard(card);
                     GameManager.instance.players[i].cardsOnTable--;
                     GameManager.instance.subTractCreature(creatureType, i);
+                    continue;
                 }
             }
         }

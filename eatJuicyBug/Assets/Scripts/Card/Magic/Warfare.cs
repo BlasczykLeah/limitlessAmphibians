d82c840919ec8a45b690e8e524146a6feb31e887
Zaidis <chewybinx@gmail.com>
2020-10-25 16:58:41 -0500

    hi

--- eatJuicyBug/Assets/Scripts/Card/Magic/Warfare.cs
+++ eatJuicyBug/Assets/Scripts/Card/Magic/Warfare.cs
@@ -7,14 +7,14 @@ public class Warfare : Magic
 
     // destroy each of creature type
     public override void DoMagic(int playerIndex, int targetIndex, CreatureType creatureType) {
-        for (int i = 0; i < GameManager.instance.tableLayouts[targetIndex].tableCards.Count; i++) {
-            if (GameManager.instance.tableLayouts[targetIndex].tableCards[i].GetComponent<CardData>().GetCreatureType() == creatureType) {
-                // if the player has the specific creature type. 
-                GameObject card = GameManager.instance.tableLayouts[targetIndex].tableCards[i];
-                GameManager.instance.tableLayouts[targetIndex].GetComponent<CustomLayout>().removePlacedCard(card);
-                GameManager.instance.players[targetIndex].cardsOnTable--;
-                GameManager.instance.subTractCreature(creatureType, playerIndex);
-                return;
+        for (int i = 0; i < GameManager.instance.tableLayouts.Length; i++) {
+            foreach (GameObject card in GameManager.instance.tableLayouts[i].tableCards) {
+                if (card.GetComponent<CardData>().GetCreatureType() == creatureType) {
+                    // if the player has the specific creature type. 
+                    GameManager.instance.tableLayouts[i].GetComponent<CustomLayout>().removePlacedCard(card);
+                    GameManager.instance.players[i].cardsOnTable--;
+                    GameManager.instance.subTractCreature(creatureType, i);
+                }
             }
         }
     }

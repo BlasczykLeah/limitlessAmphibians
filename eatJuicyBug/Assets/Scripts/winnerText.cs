e5e0c099db488f662bb6d6f3b91d1fa0fdf74480
Dan Carmichael <dscarmichael5@gmail.com>
2020-10-25 16:12:16 -0500

    winner scene

---
+++ eatJuicyBug/Assets/Scripts/winnerText.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class winnerText : MonoBehaviour
+{
+    public Text text;
+    public string winnerName;
+    // Start is called before the first frame update
+    void Start()
+    {
+        text.text = winnerName + " is \nthe champion";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

8c9907610dd1d125841166764fb1c0a47edcad45
SebaldDM13 <64290663+SebaldDM13@users.noreply.github.com>
2020-11-10 17:59:01 -0600

    Exiting game from Title when in editor

--- eatJuicyBug/Assets/Scripts/Title/Usernames.cs
+++ eatJuicyBug/Assets/Scripts/Title/Usernames.cs
@@ -89,6 +89,10 @@ public class Usernames : MonoBehaviour
 
     public void quit()
     {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
         Application.Quit();
+#endif
     }
 }
